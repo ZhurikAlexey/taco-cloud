df2710c5f17c02c5aed94ff7afacb8412a8bd645
Zhurik Alexey <zhuryk@softclub.by>
2019-06-07 18:03:52 +0300

    Changed image of Taco.

--- src/main/java/tacos/Taco.java
+++ src/main/java/tacos/Taco.java
@@ -1,11 +1,26 @@
 package tacos;
 
-import lombok.Data;
+import lombok.*;
+import org.springframework.beans.factory.annotation.Value;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.List;
 
 @Data
 public class Taco {
+    @NotNull
+    @Value("#{taco.name.min.size}")
+    static String minSize;
+    //static final String str= "Name must be at least" + minSize + " characters long";
+    @Size(min = 5, message = "Name must be at least characters long")
     private String name;
     private List ingredients;
+    public Taco() {
+    }
+
+    protected boolean canEqual(final Object other) {
+        return other instanceof Taco;
+    }
+
 }
